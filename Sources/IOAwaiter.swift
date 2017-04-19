9d3f1ac661ad6a2de6d9cd4c579467d327d48b7d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-04-19 19:26:50 +0000

    Add deadline to IOAwaiter

--- Sources/IOAwaiter.swift
+++ Sources/IOAwaiter.swift
@@ -9,11 +9,12 @@
  */
 
 import Platform
+import struct Foundation.Date
 
 public enum IOEvent {
     case read, write
 }
 
 public protocol IOAwaiter {
-    func wait(for descriptor: Descriptor, event: IOEvent) throws
+    func wait(for descriptor: Descriptor, event: IOEvent, deadline: Date) throws
 }
