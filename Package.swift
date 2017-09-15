68de37231798dcef99a741947be2ce79d93e1b81
Tony Freeman <tonyfreeman@users.noreply.github.com>
2017-09-15 21:33:17 +0000

    Bundle AsyncDispatch Generally we don't want AsyncDispatch to be bundled with Async but since we can't create global TestAsync for Tests only, we had to copy TestAsync into each test target. We also can't add the TestAsync to the Test module, because we don't want to add Async dependency to each module which depends on Test. So let's just bundle and use AsyncDispatch instead.

--- Package.swift
+++ Package.swift
@@ -14,15 +14,24 @@ import PackageDescription
 let package = Package(
     name: "Async",
     products: [
-        .library(name: "Async", targets: ["Async"])
+        .library(name: "Async", targets: ["Async"]),
+        .library(name: "AsyncDispatch", targets: ["AsyncDispatch"])
     ],
     dependencies: [
         .package(
             url: "https://github.com/tris-foundation/platform.git",
             from: "0.4.0"
+        ),
+        .package(
+            url: "https://github.com/tris-foundation/test.git",
+            from: "0.4.0"
         )
     ],
     targets: [
-        .target(name: "Async", dependencies: ["Platform"])
+        .target(name: "Async", dependencies: ["Platform"]),
+        .target(name: "AsyncDispatch", dependencies: ["Async"]),
+        .testTarget(
+            name: "AsyncDispatchTests",
+            dependencies: ["AsyncDispatch", "Test"])
     ]
 )
---
+++ Tests/AsyncDispatchTests/AsyncDispatchTests.swift
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2017 Tris Foundation and the project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License
+ *
+ * See LICENSE.txt in the project root for license information
+ * See CONTRIBUTORS.txt for the list of the project authors
+ */
+
+import Test
+import Dispatch
+import Foundation
+
+@testable import AsyncDispatch
+
+class AsyncDispatchTests: TestCase {
+    func testTask() {
+        let async = AsyncDispatch()
+        let condition = DispatchSemaphore(value: 0)
+
+        var done = false
+        async.task {
+            done = true
+            condition.signal()
+        }
+
+        condition.wait()
+        assertTrue(done)
+    }
+
+    func testSyncTask() {
+        let async = AsyncDispatch()
+
+        var done = false
+        let result: Int? = try? async.syncTask {
+            done = true
+            return 42
+        }
+
+        assertTrue(done)
+        assertEqual(result, 42)
+    }
+
+    func testSyncTaskTimeout() {
+        let async = AsyncDispatch()
+
+        var done = false
+        let task = {
+            async.sleep(until: Date(timeIntervalSinceNow: 0.2))
+            done = true
+        }
+
+        assertThrowsError(try async.syncTask(
+            deadline: Date(timeIntervalSinceNow: 0.1),
+            task: task
+        )) { error in
+            assertEqual(error as? AsyncError, .timeout)
+        }
+
+        assertFalse(done)
+    }
+
+    func testSyncTaskCancel() {
+        let async = AsyncDispatch()
+
+        var taskDone = false
+        var syncTaskDone = false
+
+        var error: Error? = nil
+
+        async.task {
+            assertNotNil(try? async.testCancel())
+            Thread.current.cancel()
+            assertNil(try? async.testCancel())
+        }
+
+        async.task {
+            do {
+                try async.syncTask {
+                    Thread.current.cancel()
+                    try async.testCancel()
+                    syncTaskDone = true
+                }
+            } catch let taskError {
+                error = taskError
+            }
+
+            taskDone = true
+        }
+
+        async.loop.run(until: Date().addingTimeInterval(0.1))
+
+        assertEqual(error as? AsyncError, .taskCanceled)
+        assertTrue(taskDone)
+        assertFalse(syncTaskDone)
+    }
+
+
+    static var allTests = [
+        ("testTask", testTask),
+        ("testSyncTask", testSyncTask),
+        ("testSyncTaskTimeout", testSyncTaskTimeout),
+        ("testSyncTaskCancel", testSyncTaskCancel)
+    ]
+}
